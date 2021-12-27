64e43b6b24bfa482393e5caf60f06bf9a678ecf5
loving-code <78284208+loving-code@users.noreply.github.com>
2021-12-27 17:48:07 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -19,11 +19,37 @@ function take_snapshot(){
 }
 
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/L-8Uj7yRW/model.json');
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/L-8Uj7yRW/model.json', modelLoaded);
 
 function modelLoaded(){
-    cosnole.log('Model Loaded');
+    console.log('Model Loaded');
 }
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+    console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        prediction=results[0].label;
+        speak();
+        if(results[0].label == "victory"){
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+        if(results[0].label == "ok"){
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "awesome"){
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+    }
+}
+
 
 function speak(){
     var synth = window.speechSynthesis;
